a96ae38c5d40df370c55602d9430632ac3d75a8b
ShanEllis <sellis18@jhmi.edu>
2016-11-23 13:15:45 -0500

    data (Colors.csv) and code (ColorAnalysis_ForGithub.Rmd) included

---
+++ ColorPalletes.R
@@ -0,0 +1,109 @@
+#  ShanColors
+#' totally and absolutely stolen from Alyssa Frazee
+#' and her RSkittleBrewer concept
+#' but instead of candy...some of my favorite color combos
+           winter =  c(iceblue=rgb(154,215,218, maxColorValue=255), 
+                    darkgrey=rgb(97,97,107, maxColorValue=255),  
+                    grey=rgb(221,220,225, maxColorValue=255), 
+                    darkblue=rgb(1,23,83, maxColorValue=255),
+                    blue=rgb(150,152,121, maxColorValue=255), 
+                    purple=rgb(102,74,151, maxColorValue=255), 
+                    teal=rgb(0,164,184, maxColorValue=255))
+         
+           fall=  c(red=rgb(105,0,0, maxColorValue=255), 
+                    lightgreen=rgb(141,159,117, maxColorValue=255),  
+                    purple=rgb(132,109,116, maxColorValue=255), 
+                    pink=rgb(195,117,117, maxColorValue=255), 
+                    blue=rgb(60,65,94, maxColorValue=255), 
+                    orange=rgb(232,169,72, maxColorValue=255), 
+                    neutral=rgb(232,214,193, maxColorValue=255),
+                    darkgreen=rgb(98,97,65, maxColorValue=255))
+
+           summer= c(red=rgb(195,53,44, maxColorValue=255),
+                    orange=rgb(255, 134,66, maxColorValue=255),   
+                    green=rgb(102,141,60, maxColorValue=255), 
+                    yellow=rgb(255,246,43, maxColorValue=255), 
+                    lightblue=rgb(60,214, 230, maxColorValue=255), 
+                    blue=rgb(0,121,150, maxColorValue=255),  
+                    sand=rgb(186,166,132, maxColorValue=255),  
+                    purple=rgb(129,108,172, maxColorValue=255)) 
+
+           spring=  c(yellow=rgb(243,244,112, maxColorValue=255),   
+                    green=rgb(166,249,154, maxColorValue=255), 
+                    turquoise=rgb(162,225,224, maxColorValue=255), 
+                    purple=rgb(188,192,255, maxColorValue=255), 
+                    pink=rgb(255,211,211, maxColorValue=255), 
+                    blue=rgb(148,192,236, maxColorValue=255), 
+                    orange=rgb(243,179,127, maxColorValue=255), 
+                    grey=rgb(221,221,221, maxColorValue=255)) 
+
+           bright= c(yellow=rgb(255,222,13, maxColorValue=255), 
+                    orange=rgb(232,121,12, maxColorValue=255),   
+                    red=rgb(222,45,38, maxColorValue=255), 
+                    green=rgb(12,189,24, maxColorValue=255), 
+                    purple=rgb(148,12,232, maxColorValue=255), 
+                    blue=rgb(58,158,234, maxColorValue=255), 
+                    pink=rgb(255,78,105, maxColorValue=255), 
+                    teal=rgb(59,196,199, maxColorValue=255))
+            greys=  c(lightgrey=rgb(238,238,238, maxColorValue=255),   
+                    grey = rgb(204,204,204, maxColorValue=255), 
+                    # darkgrey = rgb(106,106,106, maxColorValue=255), 
+                     darkestgrey =rgb(86,86,86, maxColorValue=255), 
+                    black = rgb(8,8,8, maxColorValue=255)) 
+           electric= c(blue=rgb(0,216,255, maxColorValue=255), 
+                    green=rgb(0,255,121, maxColorValue=255),   
+                    pink=rgb(255,0,225, maxColorValue=255), 
+                    darkblue=rgb(0,76,255, maxColorValue=255), 
+                    red=rgb(255,0,76, maxColorValue=255), 
+                    purple=rgb(149,0,255, maxColorValue=255), 
+                    yellow=rgb(208,255,0, maxColorValue=255), 
+                    grey=rgb(81,81,81, maxColorValue=255))  
+           blues = c(extralightblue=rgb(237,240,245, maxColorValue=255), 
+                    lightblue =rgb(173,185,211, maxColorValue=255),   
+                    blue=rgb(99,123,173, maxColorValue=255),   
+                    # darkblue=rgb(58,87,149, maxColorValue=255), 
+                    darkerblue=rgb(29,52,105, maxColorValue=255),
+                    darkestblue=rgb(11,18,36, maxColorValue=255))
+                    # extrablue=rgb(94,128,191, maxColorValue=255))
+            blues = c(extralightblue=rgb(237,240,245, maxColorValue=255), 
+                    lightblue =rgb(216,227,243, maxColorValue=255),   
+                    blue=rgb(168,190,222, maxColorValue=255),   
+                    darkblue=rgb(121,150,193, maxColorValue=255), 
+                    darkerblue=rgb(81,114,162, maxColorValue=255),
+                    darkestblue=rgb(53,87,138, maxColorValue=255),
+                    extrablue=rgb(53,61,101, maxColorValue=255))
+
+            google = c(red=rgb(214,45,32, maxColorValue=255), 
+                    orange =rgb(255,167,0, maxColorValue=255),   
+                    blue=rgb(0,87,231, maxColorValue=255),   
+                    green=rgb(0,135,68, maxColorValue=255))
+            shannon = c(eggplant = rgb(126,69,114, maxColorValue=255),
+                    emerald = rgb(47,165,80, maxColorValue=255),
+                    darkblue = rgb(25,25,112, maxColorValue=255),
+                    blue=rgb(0,87,231, maxColorValue=255),
+                    pink = rgb(206,0,83, maxColorValue=255),
+                    teal = rgb(24,141,127, maxColorValue=255),
+                    black=rgb(8,8,8,maxColorValue=255))
+
+           jewel=  c(brown=rgb(221,153,51, maxColorValue=255), 
+                    red =rgb(109,0,0, maxColorValue=255),   
+                    teal=rgb(9,138,170, maxColorValue=255), 
+                    green=rgb(31,106,73, maxColorValue=255), 
+                    deeppurple=rgb(90,23,93, maxColorValue=255), 
+                    blue=rgb(56,51,107,maxColorValue=255),
+                    pink=rgb(191,23,103, maxColorValue=255)) 
+
+           RAINBOW=
+                      c(red=   rgb(177,  0,  38, maxColorValue=255),
+                        pink=  rgb(251,154,153, maxColorValue=255),
+                        orange=rgb(253,141,  60, maxColorValue=255),
+                        yellow=rgb(255,220, 20, maxColorValue=255),
+                        green= rgb(35,139,  69, maxColorValue=255),
+                        blue=  rgb( 55,26,184, maxColorValue=255),
+                        purple=rgb(152, 78,163, maxColorValue=255),
+                        brown= rgb(100, 20, 20, maxColorValue=255))
+
+
+
+
+
